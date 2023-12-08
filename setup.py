d0935c6ee7327cae2784f2e3d2f2187e761e6ccf
Leonardo Cristella <leonardo.cristella@epfl.ch>
2023-12-08 13:35:00 +0100

    Use image python:3.8

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="densities-validation",
     author="Blue Brain Project, EPFL",
-    setup_requires=["setuptools-git-versioning"],
-    setuptools_git_versioning={'enabled': True},
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
     description=(
         "Performing a set of assertions on cell densities and its subtypes "
         "according to litterature"
     ),
     license="BBP-internal-confidential",
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     install_requires=[
         "voxcell==3.1.6",
     ],
