cf55e788d7d43f0c7d23e77d35cf6da98c751eeb
Sébastien Yannick Piluso <sebastien.piluso@epfl.ch>
2023-12-09 00:57:43 +0000

    Fix bug in inhibitory_density_folder name

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python
 from setuptools import setup, find_packages
 setup(
     name="densities-validation",
     author="Blue Brain Project, EPFL",
     setup_requires=["setuptools_scm"],
     use_scm_version=True,
+    version="v0.0.3",
     description=(
         "Performing a set of assertions on cell densities and its subtypes "
         "according to litterature"
     ),
     license="BBP-internal-confidential",
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     install_requires=[
         "voxcell==3.1.6",
     ],
-
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     entry_points={
         "console_scripts": ["densities-validation=validation:main"]
