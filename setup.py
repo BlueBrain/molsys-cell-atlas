656e589bb6b4b8ea29db429cab291f498a73e554
Leonardo Cristella <leonardo.cristella@epfl.ch>
2023-12-11 18:10:28 +0000

    Set package version automatically

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
+import os
 from setuptools import setup, find_packages
+
+COMMIT_TAG = os.environ.get("CI_COMMIT_TAG")
+
 setup(
     name="densities-validation",
     author="Blue Brain Project, EPFL",
     setup_requires=["setuptools_scm"],
-    use_scm_version=True,
-    version="v0.0.3",
+    use_scm_version=True if not COMMIT_TAG else None,
+    version=COMMIT_TAG if COMMIT_TAG else None,
     description=(
         "Performing a set of assertions on cell densities and its subtypes "
         "according to litterature"
@@ -14,9 +18,9 @@ setup(
     install_requires=[
         "voxcell==3.1.6",
     ],
-    packages=find_packages(exclude=("tests",)),
+    packages=find_packages(),
     include_package_data=True,
     entry_points={
-        "console_scripts": ["densities-validation=validation:main"]
+        "console_scripts": ["densities-validation=src.validation:main"]
     },
 )
