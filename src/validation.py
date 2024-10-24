c98d1c62c81bf20d761a7d621c7fab4ed89da088
Sébastien Yannick Piluso <sebastien.piluso@epfl.ch>
2024-10-24 10:27:41 +0000

    correction import re

--- src/validation.py
+++ src/validation.py
@@ -13,6 +13,7 @@ from argparse import ArgumentParser
 import os, sys
 import warnings
 import inspect
+import re
 from src.literature import *
 # =============================================================================================
 
